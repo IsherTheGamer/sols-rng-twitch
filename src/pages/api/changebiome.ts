190c71a1bbaef13294e3da3099d55147fbe8384c
IsherTheGamer <gurmitcanadawala@gmail.com>
2026-07-28 17:16:18 -0400

    Update changebiome.ts

--- src/pages/api/changebiome.ts
+++ src/pages/api/changebiome.ts
@@ -1,7 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import biomeHandler from "./biome";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const token = req.query.token;
+
+  if (token !== process.env.API_SECRET_TOKEN) {
+    return res.status(403).json({
+      error: "Invalid token"
+    });
+  }
+
   req.query.action = "change";
+
   return biomeHandler(req, res);
 }
